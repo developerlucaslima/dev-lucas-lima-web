24caff1dc1d968ebc872a85c9cb25bfccb37010b
developerlucaslima <developerlucaslima@gmail.com>
2025-03-29 16:27:16 -0300

    :sparkles: feat: enhance HeroSection with link to About section and improve subtitle animation

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,48 +1,39 @@
 'use client'
 
+import Link from 'next/link'
+
 import { heroConfig } from '@/config/hero-config'
 import { useFadeIn } from '@/hooks/animations/use-fade-in'
 import { useFadeSlideY } from '@/hooks/animations/use-fade-slide-y'
 import { useTypewriter } from '@/hooks/animations/use-typewriter'
 
 export function HeroSection() {
-  const { textRef, cursorRef } = useTypewriter({ texts: heroConfig.subtitles })
+  const { textRef } = useTypewriter({ texts: heroConfig.subtitles })
 
   const titleRef = useFadeSlideY({ delay: 0 })
   const subtitleRef = useFadeIn()
-  const scrollRef = useFadeIn({ delay: 2 })
 
   return (
-    <section className="flex h-screen w-full flex-col items-center justify-center overflow-hidden">
-      <div ref={titleRef} className="mb-4">
-        <h1 className="text-center text-4xl font-bold uppercase sm:text-7xl md:text-8xl">
-          {heroConfig.title}
-        </h1>
-      </div>
+    <section className="flex h-screen w-full flex-col items-center justify-center overflow-hidden text-center">
+      <Link href="#about">
+        <div ref={titleRef} className="mb-4">
+          <h1 className="text-4xl font-bold uppercase sm:text-7xl md:text-8xl">
+            {heroConfig.title}
+          </h1>
+        </div>
 
-      <div className="h-16">
-        <h2
-          ref={subtitleRef}
-          className="text-2xl font-light sm:text-3xl md:text-4xl"
-        >
-          <span>&lt;</span>
-          <span ref={textRef}></span>
-          <span ref={cursorRef}>|</span>
-          <span>/&gt;</span>
-        </h2>
-      </div>
-
-      <div ref={scrollRef} className="mt-12">
-        <a
-          href="#about"
-          className="text-muted-foreground hover:text-foreground flex flex-col items-center transition-colors duration-300"
-        >
-          <span className="mb-2 text-sm uppercase">
-            {heroConfig.scrollDown.text}
-          </span>
-          <heroConfig.scrollDown.icon className="animate-bounce" size={24} />
-        </a>
-      </div>
+        <div className="h-16">
+          <h2
+            ref={subtitleRef}
+            className="text-2xl font-light sm:text-3xl md:text-4xl"
+          >
+            <span>&lt;</span>
+            <span ref={textRef}></span>
+            <span className="animate-caret-blink">|</span>
+            <span>/&gt;</span>
+          </h2>
+        </div>
+      </Link>
     </section>
   )
 }
