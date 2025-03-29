1d8339e916af79999eb137d73303b67ef4f405c3
developerlucaslima <developerlucaslima@gmail.com>
2025-03-29 16:27:25 -0300

    :sparkles: feat: update about section with new personal details and improve card content for better clarity

--- src/config/about-config.ts
+++ src/config/about-config.ts
@@ -1,54 +1,86 @@
-import { Brain, Code2, Github, Linkedin, User } from 'lucide-react'
+import {
+  Clock,
+  Clover,
+  Code2,
+  Github,
+  HandHelping,
+  Linkedin,
+  SmilePlus,
+} from 'lucide-react'
 
 import { calculateDurationFrom } from '@/utils/calculate-duration'
 
 import { heroConfig } from './hero-config'
 
+const { duration: yearsAsDev } = calculateDurationFrom({
+  startDate: new Date(2022, 6, 1),
+})
+const { years: yearsOld } = calculateDurationFrom({
+  startDate: new Date(1995, 10, 10),
+})
+
 export const aboutConfig = {
   sectionTitle: 'About',
-  firstParagraph: `My journey as a developer began about ${calculateDurationFrom({ startDate: new Date(2022, 6, 1) })} years ago; since then, I have worked with various technologies and frameworks, specializing in frontend with JavaScript, TypeScript, React, Next, and Vue. Leveraging my design skills, I create intuitive, functional, and visually appealing interfaces.`,
-  secondParagraph:
-    'I have also frequently worked as a backend or full stack developer, delivering high-quality solutions using primarily Node but also Java, including the development and consumption of REST and GraphQL APIs. Constantly staying updated on best practices, I apply modern patterns to ensure performance, scalability, and maintainability.',
+  paragraphs: [
+    `Hello, my name is Lucas Fraga de Lima. I am ${yearsOld} years old, gaucho from Gramado - RS, and currently living in Florianópolis - SC.`,
+    `My journey as a developer began about ${yearsAsDev} years ago; since then, I have worked with various technologies and frameworks, specializing in frontend with JavaScript, TypeScript, React, Next, and Vue. Leveraging my design skills, I create intuitive, functional, and visually appealing interfaces.`,
+    'I have also frequently worked as a backend or full stack developer, delivering high-quality solutions using primarily Node but also Java, including the development and consumption of REST and GraphQL APIs. Constantly staying updated on best practices, applying modern patterns to ensure performance, scalability, and maintainability.',
+  ],
   leftCards: [
     {
-      icon: Brain,
-      title: 'Philosophy',
+      icon: SmilePlus,
+      title: 'Kaizen ',
       details: [
-        '- Kaizen (改善): continuous, incremental improvement in every aspect of life and work.',
-        '- Ikigai (生き甲斐): where passion, mission, vocation, and profession converge to give life meaning.',
+        'Continuous, incremental improvement in every aspect of life and work.',
       ],
+      footer: '改善',
     },
     {
-      icon: User,
-      title: 'Me',
+      icon: Clover,
+      title: 'Ikigai ',
       details: [
-        `- ${calculateDurationFrom({ startDate: new Date(1995, 10, 10) })} yo`,
-        '- Brazil',
-        '- Florianópolis, State of Santa Catarina',
+        'Where passion, mission, vocation, and profession converge to give life meaning.',
       ],
-      footer: [
-        {
-          href: 'https://www.linkedin.com/in/developerlucaslima',
-          target: '_blank',
-          rel: 'noopener noreferrer',
-          icon: Linkedin,
-        },
-        {
-          href: 'https://github.com/developerlucaslima',
-          target: '_blank',
-          rel: 'noopener noreferrer',
-          icon: Github,
-        },
+      footer: '生き甲斐',
+    },
+    {
+      icon: HandHelping,
+      title: 'Wa',
+      details: [
+        'Emphasizes harmony, balance, and the importance of peaceful relationships in both personal and social spheres.',
       ],
+      footer: '和',
+    },
+    {
+      icon: Clock,
+      title: 'Shikata ga nai',
+      details: [
+        'Embraces the inevitable flow of time and the acceptance of circumstances that cannot be changed.',
+      ],
+      footer: '仕方がない',
     },
   ],
   rightCard: {
     icon: Code2,
     title: 'Dev Lucas Lima',
     details: heroConfig.subtitles.map((subtitle) => `- ${subtitle}`),
+    footer: [
+      {
+        href: 'https://www.linkedin.com/in/developerlucaslima',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        icon: Linkedin,
+      },
+      {
+        href: 'https://github.com/developerlucaslima',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        icon: Github,
+      },
+    ],
   },
   image: {
-    src: '/dev-lucas-lima.jpg',
+    src: '/me.png',
     alt: 'Dev Lucas Lima',
     width: 400,
     height: 400,
