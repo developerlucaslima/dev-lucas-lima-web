34dfff0289974b47ac30df6fa23ed22aaef8858b
developerlucaslima <developerlucaslima@gmail.com>
2025-03-29 18:36:49 -0300

    :sparkles: feat: update bubblesConfig with new icons and names for improved clarity and organization

--- src/config/bubbles-config.ts
+++ src/config/bubbles-config.ts
@@ -1,24 +1,46 @@
+import { Palette } from 'lucide-react'
+import { DiDocker, DiGithub } from 'react-icons/di'
+import { FaCss3, FaNodeJs, FaReact } from 'react-icons/fa'
+import { MdHttp } from 'react-icons/md'
+import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri'
 import {
-  Box,
-  Braces,
-  Code2,
-  Database,
-  FileCode,
-  GitBranch,
-  Layers,
-  Palette,
-  Server,
-} from 'lucide-react'
+  SiGraphql,
+  SiJavascript,
+  SiPostgresql,
+  SiTypescript,
+} from 'react-icons/si'
 
 import type { BubbleProps } from '@/components/ui/bubble'
 
 export const bubblesConfig: BubbleProps[] = [
   {
+    id: 2,
+    initialX: 0,
+    initialY: 0,
+    name: 'TYPESCRIPT',
+    icon: SiTypescript,
+    onClick: function (): void {
+      throw new Error('Function not implemented.')
+    },
+    isOrganized: false,
+  },
+  {
+    id: 12,
+    initialX: 0,
+    initialY: 0,
+    name: 'JAVASCRIPT',
+    icon: SiJavascript,
+    onClick: function (): void {
+      throw new Error('Function not implemented.')
+    },
+    isOrganized: false,
+  },
+  {
     id: 0,
     initialX: 0,
     initialY: 0,
     name: 'REACT',
-    icon: Code2,
+    icon: FaReact,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
@@ -29,29 +51,19 @@ export const bubblesConfig: BubbleProps[] = [
     initialX: 0,
     initialY: 0,
     name: 'NEXT.JS',
-    icon: Braces,
+    icon: RiNextjsFill,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
     isOrganized: false,
   },
-  {
-    id: 2,
-    initialX: 0,
-    initialY: 0,
-    name: 'TYPESCRIPT',
-    icon: FileCode,
-    onClick: function (): void {
-      throw new Error('Function not implemented.')
-    },
-    isOrganized: false,
-  },
+
   {
     id: 3,
     initialX: 0,
     initialY: 0,
     name: 'NODE.JS',
-    icon: Server,
+    icon: FaNodeJs,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
@@ -73,7 +85,18 @@ export const bubblesConfig: BubbleProps[] = [
     initialX: 0,
     initialY: 0,
     name: 'TAILWIND',
-    icon: Layers,
+    icon: RiTailwindCssFill,
+    onClick: function (): void {
+      throw new Error('Function not implemented.')
+    },
+    isOrganized: false,
+  },
+  {
+    id: 11,
+    initialX: 0,
+    initialY: 0,
+    name: 'CSS',
+    icon: FaCss3,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
@@ -83,8 +106,8 @@ export const bubblesConfig: BubbleProps[] = [
     id: 6,
     initialX: 0,
     initialY: 0,
-    name: 'MONGODB',
-    icon: Database,
+    name: 'GRAPHQL',
+    icon: SiGraphql,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
@@ -94,8 +117,8 @@ export const bubblesConfig: BubbleProps[] = [
     id: 7,
     initialX: 0,
     initialY: 0,
-    name: 'GIT',
-    icon: GitBranch,
+    name: 'REST',
+    icon: MdHttp,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
@@ -105,8 +128,30 @@ export const bubblesConfig: BubbleProps[] = [
     id: 8,
     initialX: 0,
     initialY: 0,
+    name: 'POSTGRESQL',
+    icon: SiPostgresql,
+    onClick: function (): void {
+      throw new Error('Function not implemented.')
+    },
+    isOrganized: false,
+  },
+  {
+    id: 9,
+    initialX: 0,
+    initialY: 0,
+    name: 'GIT',
+    icon: DiGithub,
+    onClick: function (): void {
+      throw new Error('Function not implemented.')
+    },
+    isOrganized: false,
+  },
+  {
+    id: 10,
+    initialX: 0,
+    initialY: 0,
     name: 'DOCKER',
-    icon: Box,
+    icon: DiDocker,
     onClick: function (): void {
       throw new Error('Function not implemented.')
     },
