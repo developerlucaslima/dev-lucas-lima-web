3823734f1b0e7cec6954b43e364b3c3d356c02a8
developerlucaslima <developerlucaslima@gmail.com>
2025-03-30 15:48:24 -0300

    :sparkles: feat: update PhilosophySection to use AnimatedCard and implement staggered fade animations

--- src/components/sections/philosophy-section.tsx
+++ src/components/sections/philosophy-section.tsx
@@ -1,26 +1,22 @@
 'use client'
 
-import { useRef } from 'react'
-
 import { aboutConfig } from '@/config/about-config'
+import { useScrollFadeIn } from '@/hooks/animations/use-scroll-fade-in'
 import { useStaggerFadeSlideY } from '@/hooks/animations/use-stagger-fade-slide-y'
 
-import { CardAnimated, CardContent, CardFooter, CardTitle } from '../ui/card'
+import { AnimatedCard, CardContent, CardFooter, CardTitle } from '../ui/card'
 
 export function PhilosophySection() {
-  const philosophyRef = useRef(null)
-  const cardContainerRef = useStaggerFadeSlideY({
-    childSelector: '.philosophy-card',
-    start: 'top 50%',
-  }) as React.RefObject<HTMLDivElement>
+  // TODO : animated fade must to be one each time
+  const refs = useStaggerFadeSlideY()
+  const titleRef = useScrollFadeIn()
 
   return (
     <section
       id="philosophy"
-      ref={philosophyRef}
-      className="philosophy-section container mx-auto flex min-h-screen max-w-5xl flex-col p-16"
+      className="container mx-auto flex min-h-screen max-w-5xl flex-col p-16"
     >
-      <div className="mb-16 text-center">
+      <div ref={titleRef} className="mb-16 text-center">
         <div className="text-center">
           <h2 className="mb-4 text-2xl font-light uppercase sm:text-3xl md:text-4xl">
             Philosophy
@@ -28,19 +24,25 @@ export function PhilosophySection() {
           <div className="bg-foreground mx-auto h-0.5 w-20" />
         </div>
       </div>
-      <div
-        ref={cardContainerRef}
-        className="my-auto flex flex-wrap items-center justify-center gap-8"
-      >
-        {aboutConfig.leftCards.map((card) => (
-          <CardAnimated key={card.title} className="philosophy-card min-h-60">
-            <CardTitle>
-              <card.icon className="text-foreground mr-2" size={20} />
-              <h3 className="text-lg font-semibold uppercase">{card.title}</h3>
-            </CardTitle>
-            <CardContent className="text-center">{card.details}</CardContent>
-            <CardFooter>{card.footer}</CardFooter>
-          </CardAnimated>
+      <div className="my-auto flex flex-wrap items-center justify-center gap-8">
+        {aboutConfig.leftCards.map((card, index) => (
+          <div
+            key={card.title}
+            ref={(el) => {
+              refs.current[index] = el
+            }}
+          >
+            <AnimatedCard className="min-h-60">
+              <CardTitle>
+                <card.icon className="text-foreground mr-2" size={20} />
+                <h3 className="text-lg font-semibold uppercase">
+                  {card.title}
+                </h3>
+              </CardTitle>
+              <CardContent className="text-center">{card.details}</CardContent>
+              <CardFooter>{card.footer}</CardFooter>
+            </AnimatedCard>
+          </div>
         ))}
       </div>
     </section>
